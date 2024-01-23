ea9926c41cbbb01618c2b3dd00fb55a5bef1b3fe
ikjunchoi <choiikjun1101@gmail.com>
2024-01-24 02:03:05 +0900

    ➕ add: user dep to phrase

--- src/phrase/phrase.module.ts
+++ src/phrase/phrase.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { PhraseController } from './phrase.controller';
 import { PhraseService } from './phrase.service';
 import { PhraseRepository } from './phrase.repository';
+import { UserService } from 'src/user/user.service';
+import { UserRepository } from 'src/user/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+        signOptions: {
+          expiresIn: `${config.get('JWT_ACCESS_TOKEN_EXPIRATION_TIME')}`,
+        },
+      }),
+    }),
+    HttpModule,
+  ],
   controllers: [PhraseController],
-  providers: [PhraseService, PhraseRepository],
+  providers: [PhraseService, PhraseRepository, UserService, UserRepository],
 })
 export class PhraseModule {}
