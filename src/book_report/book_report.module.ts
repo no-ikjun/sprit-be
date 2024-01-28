40ef297687f1d64187373a989a255fcae313a921
ikjunchoi <choiikjun1101@gmail.com>
2024-01-28 16:59:51 +0900

    ➕ fix: add dep

--- src/book_report/book_report.module.ts
+++ src/book_report/book_report.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { BookReportController } from './book_report.controller';
 import { BookReportService } from './book_report.service';
 import { BookReportRepository } from './book_report.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserService } from 'src/user/user.service';
+import { UserRepository } from 'src/user/user.repository';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+        signOptions: {
+          expiresIn: `${config.get('JWT_ACCESS_TOKEN_EXPIRATION_TIME')}`,
+        },
+      }),
+    }),
+  ],
   controllers: [BookReportController],
-  providers: [BookReportService, BookReportRepository],
+  providers: [
+    BookReportService,
+    BookReportRepository,
+    UserService,
+    UserRepository,
+  ],
 })
 export class BookReportModule {}
