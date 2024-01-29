8ae0f704824a769883e2167c6d301900a0316a11
ikjunchoi <choiikjun1101@gmail.com>
2024-01-29 12:56:55 +0900

    ✨ feat: send push notification service

---
+++ src/firebase/fcm.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
+import { ConfigService } from '@nestjs/config';
+import { cert, initializeApp } from 'firebase-admin/app';
+import { getMessaging } from 'firebase-admin/messaging';
+
+@Injectable()
+export class GlobalFcmService {
+  constructor(private readonly configService: ConfigService) {
+    initializeApp({
+      credential: cert({
+        projectId: this.configService.get<string>('FCM_PROJECT_ID'),
+        privateKey: this.configService
+          .get<string>('FCM_PRIVATE_KEY')
+          .replace(/\\n/g, '\n'),
+        clientEmail: this.configService.get<string>('FCM_CLIENT_EMAIL'),
+      }),
+    });
+  }
+
+  postMessage(
+    title: string,
+    body: string,
+    imageUrl: string,
+    token: string,
+    sound = 'default',
+    data?: { [key: string]: string },
+  ) {
+    getMessaging()
+      .send({
+        notification: {
+          title,
+          body,
+          imageUrl,
+        },
+        android: {
+          notification: { sound },
+        },
+        apns: {
+          payload: { aps: { sound } },
+        },
+        token,
+        data,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
+  }
+}
