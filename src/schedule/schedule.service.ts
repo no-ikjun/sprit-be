1b4717cb6df2c0baec7a1a82469f9140f106b52b
ikjunchoi <choiikjun1101@gmail.com>
2024-03-04 09:05:41 +0900

    🎨 update: reset weekly report time

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -142,8 +142,7 @@ export class ScheduleService {
     this.timeMessage(12);
   }
 
-  //@Cron('0 10 * * 0')
-  @Cron('30 16 * * 0')
+  @Cron('0 10 * * 0')
   async handleWeeklyReport() {
     const tokens = await this.notificationRepository.getTimeAgreeFcmToken(
       this.dataSource.manager,
