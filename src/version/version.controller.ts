5c5ac0637ee7b8d1a323e2b88089eade4f4ceb1b
ikjunchoi <choiikjun1101@gmail.com>
2024-09-08 00:30:56 +0900

    🐛 fix: change url of version entity file

--- src/version/version.controller.ts
+++ src/version/version.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { VersionService } from './version.service';
 import { CreateVersionDto } from './dto/version.dto';
 import { Version } from 'src/global/entities/version.entity';
+import { JwtAccessGuard } from 'src/auth/guard/jwtAccess.guard';
 
 @Controller('v1/version')
 export class VersionController {
   constructor(private readonly versionService: VersionService) {}
 
   @Post()
+  @UseGuards(JwtAccessGuard)
   async setNewVersion(@Body() body: CreateVersionDto): Promise<void> {
     await this.versionService.setNewVersion(
       body.version_number,
@@ -18,6 +20,7 @@ export class VersionController {
   }
 
   @Get()
+  @UseGuards(JwtAccessGuard)
   async getLatestVersion(): Promise<Version> {
     return await this.versionService.getLatestVersion();
   }
--- src/version/version.module.ts
+++ src/version/version.module.ts
@@ -1,8 +1,9 @@
-import { Module, Version } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { VersionService } from './version.service';
 import { VersionController } from './version.controller';
 import { VersionRepository } from './version.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Version } from 'src/global/entities/version.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Version])],
